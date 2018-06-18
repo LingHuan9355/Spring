a9674c26a06d46f8bffd85cbce66623166592b85
MrLing <LingHuan5593@qq.com>
2018-06-18 16:35:19 +0800

    Spring1 IOC容器 依赖注入的两种方式：属性注入

--- 180615Spring1/src/com/hr/spring/beans/TestMain.java
+++ 180615Spring1/src/com/hr/spring/beans/TestMain.java
@@ -16,13 +16,24 @@ public class TestMain {
 				   //heWorld.hello();
 				   
 				   //1.创建Spring 的 IOC 容器对象
-				   @SuppressWarnings("resource")
-				ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+					 //ApplicationContext 代表IOC 容器
+					 //ClassPathXmlApplicationContext :是ApplicationContext 接口的实现类，该实现类从类路径下来加载配置文件。
+				   ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 				   
 				   //2.从IOC 容器中获取Bean实例
-			      HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
+				   //利用id 定位到IOC容器中的bean
+			      HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld2");
+				  //利用类型返回 IOC 容器中的Bean ，但要求IOC容器中只能有一个该类型的Bean
+			      //HelloWorld helloWorld =  context.getBean(HelloWorld.class);
+			      System.out.println(helloWorld);
 				   
 				   //3.调用hello的方法
 			    //  helloWorld.hello();
+			      
+			      Car car = (Car) context.getBean("car");
+			      System.out.println(car);
+	
+			      car  = (Car) context.getBean("car2");
+			      System.out.println(car);
 				 }
 }
